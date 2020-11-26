a9c839cca2f2b21f95d0eba23c31c05755c7f2b1
aisha01malik <73873190+aisha01malik@users.noreply.github.com>
2020-11-26 15:32:01 +0530

    Update extra_integer_factory.h

--- C-Big-Integer-Library/C++ Big Integer Library/extra_integer_factory.h
+++ C-Big-Integer-Library/C++ Big Integer Library/extra_integer_factory.h
@@ -1,10 +1,7 @@
 //
 //  extra_integer_factory.h
 //  C++ Big Integer Library
-//
-//  Created by Mahmud on 10/29/18.
-//  Copyright © 2018 Mahmud. All rights reserved.
-//
+
 
 #ifndef extra_integer_factory_h
 #define extra_integer_factory_h
